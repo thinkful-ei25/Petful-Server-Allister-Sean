2caef25773e817be3cc48eb86776ef26a82c49c7
Sean Nealon <signalflowsean@gmail.com>
2018-12-21 11:10:37 -0500

    get endpoints for both cat and dog added

--- index.js
+++ index.js
@@ -5,10 +5,31 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
 
 const app = express();
+
+const dogs = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }, 
+]; 
+const cats = [ 
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+]; 
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -22,6 +43,18 @@ app.use(
   })
 );
 
+app.get('/api/cat', (req, res) => { 
+  
+  //returns first cat
+  res.json(cats[0]); 
+}); 
+
+app.get('/api/dog', (req, res) => { 
+
+  //returns first dog
+  res.json(dogs[0]); 
+}); 
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
@@ -34,7 +67,6 @@ function runServer(port = PORT) {
 }
 
 if (require.main === module) {
-  dbConnect();
   runServer();
 }
 
